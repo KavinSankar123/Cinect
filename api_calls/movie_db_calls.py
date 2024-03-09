92d692b2741822a982eba1bf636a9e53a5f3e851
KavinSPitt <91901198+KavinSPitt@users.noreply.github.com>
2024-03-08 21:51:33 -0500

    added code to properly import scraper dir

--- api_calls/movie_db_calls.py
+++ api_calls/movie_db_calls.py
@@ -1,12 +1,8 @@
-import scraper
+import sys
+import os
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+scraper_dir = os.path.join(current_dir, '..', 'scraper')
+sys.path.append(scraper_dir)
 
 
-def main():
-    username = "KavinTheCabbage"
-    res = scraper.scraper.scrape_list(username)
-
-
-    print(res)
-
-
-main()
